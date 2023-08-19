8f71b7b7441d32a9a40d3922ecb7ce673a73075a
ECUsam <1254281150@qq.com>
2023-08-19 09:54:22 +0800

    解密完成

--- text_watermark/extract.py
+++ text_watermark/extract.py
@@ -1,13 +1,43 @@
+import os.path
+
 import numpy as np
 from cv2 import dct
 from numpy.linalg import svd
 
-from text_watermark.functions import text_core_function, random_strategy1
+from text_watermark.functions import text_core_function, random_strategy1, read_img
+
+
+def one_dim_kmeans(inputs):
+    threshold = 0
+    e_tol = 10 ** (-6)
+    center = [inputs.min(), inputs.max()]  # 1. 初始化中心点
+    for i in range(300):
+        threshold = (center[0] + center[1]) / 2
+        is_class01 = inputs > threshold  # 2. 检查所有点与这k个点之间的距离，每个点归类到最近的中心
+        center = [inputs[~is_class01].mean(), inputs[is_class01].mean()]  # 3. 重新找中心点
+        if np.abs((center[0] + center[1]) / 2 - threshold) < e_tol:  # 4. 停止条件
+            threshold = (center[0] + center[1]) / 2
+            break
+
+    is_class01 = inputs > threshold
+    return is_class01
 
 
 class extractor(text_core_function):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, encoding='gbk'):
+        super().__init__(encoding=encoding)
+
+
+    def extract_form_file(self, filename=None):
+        assert os.path.exists(filename), '文件不存在'
+        self.ex_img = read_img(filename)
+        wm_shape = 240
+        wm_avg = self.extract_with_kmeans(img=self.ex_img, wm_shape=wm_shape)
+        wm = self.extract_decrypt(wm_avg=wm_avg)
+
+        byte = ''.join(str((i >= 0.5) * 1) for i in wm)
+        wm = bytes.fromhex(hex(int(byte, base=2))[2:]).decode(self.encoding, errors='replace')
+        print(wm)
 
     def one_block_get_wm(self, args):
         block, shuffler = args
@@ -22,7 +52,6 @@ class extractor(text_core_function):
 
     def extract_bit_from_img(self, img):
         self.read_img_to_arr(img=img)
-        self.init_block_index()
 
         wm_block_bit = np.zeros(shape=(3, self.block_num))
         self.idx_shuffle = random_strategy1(seed=self.password,
@@ -36,3 +65,35 @@ class extractor(text_core_function):
                                                        self.idx_shuffle[i])
                                                       for i in range(self.block_num)])
         return wm_block_bit
+
+    def extract_avg(self, wm_block_bit):
+        # 对循环嵌入+3个 channel 求平均
+        wm_avg = np.zeros(shape=self.wm_size)
+        for i in range(self.wm_size):
+            wm_avg[i] = wm_block_bit[:, i::self.wm_size].mean()
+        return wm_avg
+
+    def extract(self, img, wm_shape):
+        self.wm_size = np.array(wm_shape).prod()
+
+        # 提取每个分块埋入的 bit：
+        wm_block_bit = self.extract_bit_from_img(img=img)
+        # 做平均：
+        wm_avg = self.extract_avg(wm_block_bit)
+        return wm_avg
+
+    def extract_with_kmeans(self, img, wm_shape):
+        wm_avg = self.extract(img=img, wm_shape=wm_shape)
+
+        return one_dim_kmeans(wm_avg)
+
+    def extract_decrypt(self, wm_avg):
+        wm_index = np.arange(self.wm_size)
+        np.random.RandomState(self.password).shuffle(wm_index)
+        wm_avg[wm_index] = wm_avg.copy()
+        return wm_avg
+
+
+if __name__ == "__main__":
+    a = extractor(encoding='utf-8')
+    a.extract_form_file('test.jpg')
--- text_watermark/functions.py
+++ text_watermark/functions.py
@@ -30,7 +30,8 @@ def random_strategy2(seed, size, block_shape):
 
 class text_core_function:
     def __init__(self, password=1, mode='str', encoding='gbk'):  # gbk编码省空间
-        self.wm_size = None
+        self.fast_mode = False
+        self.wm_size = 0
         self.password = password
         self.img = None
         self.img_YUV = None  # 采用YUV通道,默认不透明图像
@@ -46,6 +47,7 @@ class text_core_function:
         self.inited = False
         self.ll_part = [np.array([])] * 3  # 四维分块后，有时因不整除而少一部分，self.ca_part 是少这一部分的 self.ca
         self.d1, self.d2 = 36, 20  # d1/d2 越大鲁棒性越强,但输出图片的失真越大
+        self.pool = pool.AutoPool(mode='common', processes=None)
 
     def init_emb_func(self, filename, wm_content):
         self.img = read_img(filename).astype(np.float32)
@@ -59,18 +61,25 @@ class text_core_function:
         if self.mode == 'str':
             byte = bin(int(self.wm_content.encode(self.encoding).hex(), base=16))[2:]
             self.wm_bit = (np.array(list(byte)) == '1')
+
+        np.random.RandomState(self.password).shuffle(self.wm_bit)
+
         self.wm_size = self.wm_bit.size
 
     def read_img_to_arr(self, img: numpy.ndarray):
         """
         img: 主要提供类提示
         下面一句，补一条白边变为偶数像素，不过原题给的偶数像素所以
-        self.img_YUV = cv2.copyMakeBorder(cv2.cvtColor(img, cv2.COLOR_RGB2YUV),
+        self.img_YUV = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)  # Convert RGB to YUV
+        """
+
+
+        self.img_shape = img.shape[:2]
+        self.img_YUV = cv2.copyMakeBorder(cv2.cvtColor(img, cv2.COLOR_BGR2YUV),
                                   0, img.shape[0] % 2, 0, img.shape[1] % 2,
                                   cv2.BORDER_CONSTANT, value=(0, 0, 0))
-        """
-        self.img_YUV = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)  # Convert RGB to YUV
-        self.img_shape = self.img_YUV.shape[:2]
+
+
         self.ll_shape = [i // 2 for i in self.img_shape]
         self.ll_block_shape = (
             self.ll_shape[0] // self.block_shape[0], self.ll_shape[1] // self.block_shape[1], self.block_shape[0],
@@ -83,36 +92,21 @@ class text_core_function:
             # 使用滑动窗口把ll部分一分为四
             self.ll_block[channel] = np.lib.stride_tricks.as_strided(self.ll[channel].astype(np.float32),
                                                                      self.ll_block_shape, strides)
+
         self.init_block_index()
+
 
     def embed_func(self):
         assert self.inited is True, "未初始化，请使用init_class_func方法"
+
         embed_ca = copy.deepcopy(self.ll)
         embed_YUV = [np.array([])] * 3
         self.idx_shuffle = random_strategy1(self.password, self.block_num, self.block_shape[0] * self.block_shape[1])
-        # 想不用pool多进程跑来着，但是这段会导致通道错误，难受
-        """
-        tmp = []
-        # 这里可以用并行处理
-        for channel in range(3):
-            for i in range(self.block_num):
-                result = self.block_add_watermark((self.ll_block[channel][self.block_index[i]], self.idx_shuffle[i], i))
-                tmp.append(result)
-                self.ll_block[channel][self.block_index[i]] = tmp[i]
-            # 把四个分块链接回一个块
-            self.ll_part[channel] = np.concatenate([np.concatenate(row, axis=1) for row in self.ll_block[channel]], axis=0)
-            # 4维分块时右边和下边不能整除的长条保留，其余是主体部分，换成 embed 之后的频域的数据
-            embed_ca[channel][:self.part_shape[0], :self.part_shape[1]] = self.ll_part[channel]
-            # 逆变换回去
-            embed_YUV[channel] = idwt2((embed_ca[channel], self.hvd[channel]), "haar")
-        """
 
-        self.pool = pool.AutoPool(mode='common', processes=None)
         for channel in range(3):
-            tmp = self.pool.map(self.block_add_watermark,
+            tmp = self.pool.map(self.block_add_wm,
                                 [(self.ll_block[channel][self.block_index[i]], self.idx_shuffle[i], i)
                                  for i in range(self.block_num)])
-
             for i in range(self.block_num):
                 self.ll_block[channel][self.block_index[i]] = tmp[i]
 
@@ -130,7 +124,43 @@ class text_core_function:
         embed_img_YUV = embed_img_YUV[:self.img_shape[0], :self.img_shape[1]]
         embed_img = cv2.cvtColor(embed_img_YUV, cv2.COLOR_YUV2BGR)
         embed_img = np.clip(embed_img, a_min=0, a_max=255)
+
         return embed_img
+    
+    def block_add_wm(self, arg):
+        if self.fast_mode:
+            return self.block_add_wm_fast(arg)
+        else:
+            return self.block_add_wm_slow(arg)
+
+
+    def block_add_wm_fast(self, arg):
+        # dct->svd->打水印->逆svd->逆dct
+        block, shuffler, i = arg
+        wm_1 = self.wm_bit[i % self.wm_size]
+
+        u, s, v = svd(cv2.dct(block))
+        s[0] = (s[0] // self.d1 + 1 / 4 + 1 / 2 * wm_1) * self.d1
+
+        return cv2.idct(np.dot(u, np.dot(np.diag(s), v)))
+
+    def block_add_wm_slow(self, arg):
+        block, shuffler, i = arg
+        # dct->(flatten->加密->逆flatten)->svd->打水印->逆svd->(flatten->解密->逆flatten)->逆dct
+        wm_1 = self.wm_bit[i % self.wm_size]
+
+        block_dct = cv2.dct(block)
+
+        # 加密（打乱顺序）
+        block_dct_shuffled = block_dct.flatten()[shuffler].reshape(self.block_shape)
+        u, s, v = svd(block_dct_shuffled)
+        s[0] = (s[0] // self.d1 + 1 / 4 + 1 / 2 * wm_1) * self.d1
+        if self.d2:
+            s[1] = (s[1] // self.d2 + 1 / 4 + 1 / 2 * wm_1) * self.d2
+
+        block_dct_flatten = np.dot(u, np.dot(np.diag(s), v)).flatten()
+        block_dct_flatten[shuffler] = block_dct_flatten.copy()
+        return cv2.idct(block_dct_flatten.reshape(self.block_shape))
 
     def embed(self, filename=None, compression_ratio=None):
         embed_img = self.embed_func()
@@ -148,7 +178,7 @@ class text_core_function:
 
     def init_block_index(self):
         self.block_num = self.ll_block_shape[0] * self.ll_block_shape[1]
-        print(self.block_num, self.wm_size)
+        print('水印大小', self.wm_size)
         assert self.wm_size < self.block_num, IndexError(
             '最多可嵌入{}kb信息，多于水印的{}kb信息，溢出'.format(self.block_num / 1000, self.wm_size / 1000))
         print('最多可嵌入{}kb信息'.format(self.block_num / 1000))
@@ -156,38 +186,19 @@ class text_core_function:
         self.part_shape = self.ll_block_shape[:2] * self.block_shape
         self.block_index = [(i, j) for i in range(self.ll_block_shape[0]) for j in range(self.ll_block_shape[1])]
 
-    def block_add_watermark(self, arg):
-        block, shuffler, i = arg
-        wm_1 = self.wm_bit[i % self.wm_size]
-        # 分块进行dct变换
-        block_dct = cv2.dct(block)
-        # 使用shuffler打乱block_dct的顺序
-        block_dct_shuffled = block_dct.flatten()[shuffler].reshape(self.block_shape)
-        # 对打乱后的DCT块进行奇异值分解 (SVD)，得到u, s, v三个矩阵。
-        u, s, v = svd(block_dct_shuffled)
-        # 修改s[0]（直流分量）的值以嵌入wm_1信息。
-        s[0] = (s[0] // self.d1 + 1 / 4 + 1 / 2 * wm_1) * self.d1
-        if self.d2:
-            s[1] = (s[1] // self.d2 + 1 / 4 + 1 / 2 * wm_1) * self.d2
-        # 使用u, s, v重构block_dct。
-        block_dct_flatten = np.dot(u, np.dot(np.diag(s), v)).flatten()
-        # 再次使用shuffler对block_dct_flatten进行重新排序，解密回原始顺序。
-        block_dct_flatten[shuffler] = block_dct_flatten.copy()
-        # 使用逆离散余弦变换 (IDCT) 将block_dct_flatten转换回空间域，并返回。
-        return cv2.idct(block_dct_flatten.reshape(self.block_shape))
+
 
     def test_info(self):
         print(self.ll_shape, self.img_shape, self.ll_block_shape, self.block_shape)
         data = bin(int(self.wm_content.encode(self.encoding).hex(), base=16))[2:]
         str_ = bytes.fromhex(hex(int(data, 2))[2:]).decode(self.encoding)
-        self.wm_bit = (np.array(list(data)) == '1')
         self.wm_size = self.wm_bit.size
         print(str_)
         print(self.wm_size)
 
 
 if __name__ == "__main__":
-    a = text_core_function(encoding='gbk')
+    a = text_core_function(encoding='utf-8')
     a.init_emb_func("Bque.jpg", '深圳杯数学建模挑战赛')
     a.test_info()
     a.embed(filename='test.jpg')
